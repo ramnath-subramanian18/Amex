e7f8420cab841f2670256c66da463c3f19ff5018
Ramnath Subramanian <ramnath@Ramnaths-MacBook-Air.local>
2023-09-16 23:24:47 -0500

    dollar_line in list

--- pdf_to_text.py
+++ pdf_to_text.py
@@ -1,15 +1,19 @@
-# importing required modules
 from PyPDF2 import PdfReader
-
-# creating a pdf reader object
+import datefinder
+import re
+from dateutil.parser import parse
 reader = PdfReader('example2.pdf')
-
-# printing number of pages in pdf file
 print(len(reader.pages))
-
-# getting a specific page from the pdf file
-page = reader.pages[2]
-
-# extracting text from page
+page = reader.pages[3]
 text = page.extract_text()
 print(text)
+
+
+date_strings = re.findall(r'\d{2}/\d{2}/\d{2}', text)
+pattern_amount = r'\d{2}/\d{2}/\d{2}.*?(\$\d+\.\d{2})'
+pattern_line = r'\d{2}/\d{2}/\d{2}.*?(?=\n|$)'
+matches_amount = re.findall(pattern_amount, text, re.DOTALL)# match new line char as well
+matches_line = re.findall(pattern_line, text)
+print((date_strings))
+print((matches_amount))
+print(matches_line)
