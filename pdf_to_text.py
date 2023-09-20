8f20b1fda52e5d499426d99e5bd9cb5a57c2a6d1
Ramnath Subramanian <ramnath@Ramnaths-MacBook-Air.local>
2023-09-20 16:12:22 -0500

    new commit

--- pdf_to_text.py
+++ pdf_to_text.py
@@ -3,17 +3,31 @@ import datefinder
 import re
 from dateutil.parser import parse
 reader = PdfReader('example2.pdf')
-print(len(reader.pages))
-page = reader.pages[3]
+# print(len(reader.pages))
+page = reader.pages[2]
 text = page.extract_text()
-print(text)
+# print('first')
+# print(text[229:267])
+# print(text[276:335])
 
+# Define a regular expression pattern to match dates in MM/DD/YY format
+date_pattern = r'\d{2}/\d{2}/\d{2}'
 
-date_strings = re.findall(r'\d{2}/\d{2}/\d{2}', text)
-pattern_amount = r'\d{2}/\d{2}/\d{2}.*?(\$\d+\.\d{2})'
-pattern_line = r'\d{2}/\d{2}/\d{2}.*?(?=\n|$)'
-matches_amount = re.findall(pattern_amount, text, re.DOTALL)# match new line char as well
-matches_line = re.findall(pattern_line, text)
-print((date_strings))
-print((matches_amount))
-print(matches_line)
+# Find all matches of the date pattern in the text
+date_matches = re.finditer(date_pattern, text)
+
+# Iterate through the matches and extract the amounts for each date
+for match in date_matches:
+    date = match.group()
+    start_position = match.end() + 1  # Move past the date and one extra character
+    # Search for numeric values (amount) after the date
+    amount_match = re.search(r'-?\$[\d,.]+', text[start_position:])#\$ to find the $
+    amount_match_end=amount_match.end()+1+start_position
+    if amount_match:
+        amount = amount_match.group()
+    else:
+        amount = "Amount not found"
+    
+    print(f"Date: {date}, Amount: {amount},Text:{text[start_position:amount_match_end]}")
+    # print(start_position)
+    # print(amount_match_end)
