bfc82f0ffa10e248b2fa71f1aaff96a349bc3a9c
Ramnath Subramanian <ramnath@Ramnaths-MacBook-Air.local>
2023-09-28 17:53:08 -0500

    new commit after using revert

--- pdf_to_text.py
+++ pdf_to_text.py
@@ -3,8 +3,12 @@ import datefinder
 import re
 from dateutil.parser import parse
 reader = PdfReader('example2.pdf')
-# print(len(reader.pages))
-page = reader.pages[2]
+print(len(reader.pages))
+page = reader.pages[3]
 text = page.extract_text()
 print(text)
-print("hello")
+
+
+date_patterns = re.finditer(r"\d\d\/\d\d\/\d\d", text)
+
+
